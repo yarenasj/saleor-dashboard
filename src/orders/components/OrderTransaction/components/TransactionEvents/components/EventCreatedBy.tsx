5ac30ebde02ac5d9f4e2b30c142da1ad0f3af05f
yarenas <ysasarenas@gmail.com>
2025-10-20 22:03:54 -0500

    fixes

--- src/orders/components/OrderTransaction/components/TransactionEvents/components/EventCreatedBy.tsx
+++ src/orders/components/OrderTransaction/components/TransactionEvents/components/EventCreatedBy.tsx
@@ -1,6 +1,7 @@
 import { UserAvatar } from "@dashboard/components/UserAvatar";
 import { AppAvatarFragment, StaffMemberAvatarFragment } from "@dashboard/graphql";
 import { getUserInitials, getUserName } from "@dashboard/misc";
+import { staffMemberDetailsUrl } from "@dashboard/staff/urls";
 import { Box } from "@saleor/macaw-ui-next";
 import React from "react";
 import { Link } from "react-router-dom";
