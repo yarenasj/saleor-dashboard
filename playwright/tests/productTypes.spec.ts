a50822aa9835f0ac8d35220c5a431a7fc778ec44
yarenas <ysasarenas@gmail.com>
2025-11-10 14:24:14 -0500

    fixes e2e tests

--- playwright/tests/productTypes.spec.ts
+++ playwright/tests/productTypes.spec.ts
@@ -21,19 +21,15 @@ test("TC: SALEOR_1 Create basic product type #e2e #product-type", async ({ page 
 });
 test("TC: SALEOR_2 Create gift card product type #e2e #product-type", async ({ page }) => {
   const productTypePage = new ProductTypePage(page);
-
-  await productTypePage.gotoAddProductTypePage();
-  await productTypePage.typeProductTypeName(productTypeName);
-  await productTypePage.selectGiftCardButton();
-  await productTypePage.clickSaveButton();
-  await productTypePage.expectSuccessBanner();
+  await createBasicProductType(productTypePage, productTypeName);
   await expect(productTypePage.nameInput).toHaveValue(productTypeName);
 });
 test("TC: SALEOR_184 As a admin I can edit product type #e2e #product-type", async ({ page }) => {
   const productTypePage = new ProductTypePage(page);
   const updatedProductTypeName = `updated-e2e-product-type-${faker.datatype.number()}`;
 
-  await productTypePage.gotoExistingProductTypePage(PRODUCT_TYPES.productTypeToBeEdited.id);
+  const productTypeId = await createBasicProductType(productTypePage, productTypeName);
+  await productTypePage.gotoExistingProductTypePage(productTypeId);
   await productTypePage.updateProductTypeName(updatedProductTypeName);
   await productTypePage.makeProductShippableWithWeight();
   await productTypePage.clickSaveButton();
@@ -42,15 +38,15 @@ test("TC: SALEOR_184 As a admin I can edit product type #e2e #product-type", asy
   await expect(productTypePage.shippingWeightInput).toHaveValue("10");
   await expect(productTypePage.nameInput).toHaveValue(updatedProductTypeName);
 });
-test("TC: SALEOR_185 As a admin user I can delete product type with assigned products #e2e #product-type", async ({
+test("TC: SALEOR_185 As a admin user I can delete product type #e2e #product-type", async ({
   page,
 }) => {
   const productTypePage = new ProductTypePage(page);
-  const productTypeName = PRODUCT_TYPES.productTypeToBeRemoved.name;
+  const productTypeName = `e2e-product-type-${faker.datatype.number()}`;
+  const productTypeId = await createBasicProductType(productTypePage, productTypeName);
 
-  await productTypePage.gotoExistingProductTypePage(PRODUCT_TYPES.productTypeToBeRemoved.id);
+  await productTypePage.gotoExistingProductTypePage(productTypeId);
   await productTypePage.clickDeleteButton();
-  await productTypePage.deleteProductTypeDialog.clickConfirmDeletionCheckbox();
   await productTypePage.deleteProductTypeDialog.clickConfirmDeleteButton();
   await productTypePage.expectSuccessBanner();
   await productTypePage.productTypeList.waitFor({
@@ -63,12 +59,13 @@ test("TC: SALEOR_186 As a admin user I can delete several product types #e2e #pr
   page,
 }) => {
   const productTypePage = new ProductTypePage(page);
-  const rowsToBeDeleted = PRODUCT_TYPES.productTypesToBeBulkDeleted.ids;
-  const productTypeNames = PRODUCT_TYPES.productTypesToBeBulkDeleted.names;
+  const productTypeName2 = `e2e-product-type-${faker.datatype.number()}`;
+  let productTypeId = await createBasicProductType(productTypePage, productTypeName2);
+  productTypeId = productTypeId.replace("%3D", "=");
 
   await productTypePage.gotoProductTypeListPage();
   await expect(productTypePage.productTypeList).toBeVisible();
-  await productTypePage.checkProductTypesOnList(rowsToBeDeleted);
+  await productTypePage.checkProductTypesOnList([productTypeId]);
   await productTypePage.clickBulkDeleteButton();
   await productTypePage.deleteProductTypeDialog.clickConfirmDeleteButton();
   await productTypePage.expectSuccessBanner();
@@ -76,6 +73,17 @@ test("TC: SALEOR_186 As a admin user I can delete several product types #e2e #pr
     state: "visible",
     timeout: 50000,
   });
-  await expect(productTypePage.productTypeList).not.toContainText(productTypeNames[0]);
-  await expect(productTypePage.productTypeList).not.toContainText(productTypeNames[1]);
+  await expect(productTypePage.productTypeList).not.toContainText(productTypeName2);
 });
+
+async function createBasicProductType(productTypePage: ProductTypePage, productTypeName: string) {
+  await productTypePage.gotoAddProductTypePage();
+  await productTypePage.typeProductTypeName(productTypeName);
+  await productTypePage.selectGiftCardButton();
+  await productTypePage.clickSaveButton();
+  await productTypePage.expectSuccessBanner();
+  const currentUrl = productTypePage.page.url();
+  const urlObject = new URL(currentUrl);
+  const pathSegments = urlObject.pathname.split("/");
+  return pathSegments.filter(Boolean).pop() || "test";
+}
